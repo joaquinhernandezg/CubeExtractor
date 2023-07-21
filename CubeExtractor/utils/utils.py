df72dfaafb58b94b6e7c75896fadaadc1480d932
Joaquin Hernández <joaquin.hernandezg@uc.cl>
2023-07-21 11:10:08 -0400

    white

--- CubeExtractor/utils/utils.py
+++ CubeExtractor/utils/utils.py
@@ -19,7 +19,7 @@ def make_white_image(cube, out_filename=None, mask_nans=True, wave_min=None, wav
         cube = cube.select_lambda(wave_min, wave_max)
 
     white = cube.sum(axis=0)
-    white.var[white.var==0] = 0
+    white.var[white.var==0] = np.inf
     if isinstance(out_filename, str):
         white.write(out_filename )
     return white
