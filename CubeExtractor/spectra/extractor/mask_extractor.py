dc31e0fc08bdc4f9178cd620ce3206a0f1b48ee3
Joaquin Hernández <joaquin.hernandezg@uc.cl>
2023-08-26 17:32:25 -0400

    add kwargs to mask extractor

--- CubeExtractor/spectra/extractor/mask_extractor.py
+++ CubeExtractor/spectra/extractor/mask_extractor.py
@@ -5,11 +5,13 @@ import numpy as np
 
 class MaskExtractor(ApertureExtractor):
     @classmethod
-    def extract_single(cls, cube, white, ra, dec, spectrum_id, combine_method="sum", weight_method=None, segmentation_mask=None):
+    def extract_single(cls, cube, white, ra, dec, spectrum_id, combine_method="sum",
+                       weight_method=None, segmentation_mask=None,*args, **kwargs):
         mask = cls.build_mask(cube, sky_aperture=None, segmentation_mask=segmentation_mask)
         subcube = cls.subcube_from_mask(cube, mask)
         subwhite = cls.subwhite_from_mask(white, mask)
-        spec, weights = SimpleCubeSpectraCombiner.combine(subcube, subwhite, combine_method=combine_method, weighter=weight_method)
+        spec, weights = SimpleCubeSpectraCombiner.combine(subcube, subwhite,
+                                                          combine_method=combine_method, weighter=weight_method)
         if spec is None or weights is None:
             return None
         spec.primary_header["RA_OBJ"] = ra
